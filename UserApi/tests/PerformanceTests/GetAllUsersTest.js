aa9f569741f659c4d9a11851b713225650f9fbe9
onofre.junior <onofre.junior@gmail.com>
2025-04-27 16:49:56 -0300

    test: Fixed performance test script for get all users endpoint

--- UserApi/tests/PerformanceTests/GetAllUsersTest.js
+++ UserApi/tests/PerformanceTests/GetAllUsersTest.js
@@ -2,16 +2,15 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '2s',
+    vus: 10,
+    duration: '2s',
 };
 
 export default function () {
-  const res = http.get('https://localhost:55539/users', { 
-    insecureSkipTLSVerify: true,
-  });
+    const res = http.get('http://localhost:55540/users', {
+    });
 
-  check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
 }
